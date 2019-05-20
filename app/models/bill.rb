d82c6e2f1596ed1eec7f239ef78811029a588cca
hoangmy121097 <hoangmy121097@gmail.com>
2019-05-20 10:35:49 +0700

    update

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,7 +1,7 @@
 class Bill < ApplicationRecord
     has_many :line_items, dependent: :destroy
     validates :customer_name, :customer_address, :delivery_date, :email, presence: true
-    validates :customer_phone, numericality: true, length: { is: 10 }, :format => { with: /\A[0-9\s-]+\z/ }
+    
 
     def add_line_items_from_cart(cart)
         cart.line_items.each do |item|
